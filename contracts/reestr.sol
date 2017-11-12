c59948b21b0eee44232263d6ef95d4e461e6a243
devmanio <ivan.kocheshev@gmail.com>
2017-11-12 14:55:31 +0300

    getter  added

--- contracts/reestr.sol
+++ contracts/reestr.sol
@@ -16,15 +16,17 @@ contract Reestr {
 
   event _Record(uint guid, uint data, uint value);
 
-  function Reestr() {
-      owner = msg.sender ;
-  }
+function Reestr(){
+    owner = msg.sender;
+}
+
   function newRecord(uint _guid, uint _data, uint _value) ownable {
       Records[_guid] = Record(_data , _value );
       _Record(_guid, _data, _value);
   }
 
-  function getRecord(uint guid) returns (Record) {
-    return Records[guid];
+  function getRecord(uint guid) returns (uint _data, uint _value) {
+    _data  = Records[guid].data;
+    _value = Records[guid].value;
   }
 }
